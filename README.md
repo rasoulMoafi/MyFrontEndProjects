9dc5c5358ba414c4614fefa05f4c5abb1ce3201e
rasoul <rasoulhk8@gmail.com>
2023-02-08 18:43:44 +0330

    adding BookStore pro

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # MyFrontEndProjects
+
 My frontend demo projects
+
+## Book Store
+
+https://seyedrasoulmoafi.github.io/MyFrontEndProjects/book_store/
 
 ## Admin panel
 
 https://seyedrasoulmoafi.github.io/MyFrontEndProjects/admin/
 
 ## Clothes store
+
 https://seyedrasoulmoafi.github.io/MyFrontEndProjects/clothes_store/
+
+## Animal support
+
+https://seyedrasoulmoafi.github.io/MyFrontEndProjects/animal-support/
+
+## Sofa store
+
+https://seyedrasoulmoafi.github.io/MyFrontEndProjects/sofa_store/
+
+## Coffee shop
+
+https://seyedrasoulmoafi.github.io/MyFrontEndProjects/coffee-shop/
---
+++ book_store/js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $(".hamburger-menu").click(function () {
+    $(".navigation").toggleClass("change");
+  });
+  lightbox.option({
+    // 'resizeDuration': 200,
+    wrapAround: true,
+  });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    // console.log(position)
+    if (position >= 350) {
+      $(".gallery").addClass("change");
+    } else {
+      $(".gallery").removeClass("change");
+    }
+  });
+
+  $(".writers-accordion").click(function (event) {
+    // console.log('Hello')
+    // console.log(event.target.id.split('-'))
+    if (event.target.id.split("-")[0] === "button") {
+      $("#Book-1").attr(
+        "src",
+        "../img/famous_writters/" + event.target.id.split("-")[1] + "Book1.png"
+      );
+      $("#Book-2").attr(
+        "src",
+        "../img/famous_writters/" + event.target.id.split("-")[1] + "Book2.png"
+      );
+    }
+  });
+});
