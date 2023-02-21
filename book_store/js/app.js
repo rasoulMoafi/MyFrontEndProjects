2d47938b0251a47b0fc0082073feaf85dbfee701
rasoul <rasoulhk8@gmail.com>
2023-02-21 07:32:27 +0330

    modify app.js

--- book_store/js/app.js
+++ book_store/js/app.js
@@ -23,11 +23,12 @@ $(document).ready(function () {
     if (event.target.id.split("-")[0] === "button") {
       $("#Book-1").attr(
         "src",
-        "../img/famous_writters/" + event.target.id.split("-")[1] + "Book1.png"
+        "img/famous_writters/" + event.target.id.split("-")[1] + "Book1.png"
       );
+      // console.log(event.target.id.split("-")[1])
       $("#Book-2").attr(
         "src",
-        "../img/famous_writters/" + event.target.id.split("-")[1] + "Book2.png"
+        "img/famous_writters/" + event.target.id.split("-")[1] + "Book2.png"
       );
     }
   });
